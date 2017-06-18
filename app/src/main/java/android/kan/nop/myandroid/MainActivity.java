5392385d5b4df8203db13e8ca535752653747bc9
unnop <ukangkan@yahoo.com>
2017-06-18 10:37:42 +0700

    day 2 Intent to NewRegister

--- app/src/main/java/android/kan/nop/myandroid/MainActivity.java
+++ app/src/main/java/android/kan/nop/myandroid/MainActivity.java
@@ -1,13 +1,51 @@
 package android.kan.nop.myandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Explicit ประกาศตัวแปร
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Initial View
+        initialView();
+
+        // TextView Controller
+        textViewController();
+
+    }   // Method Main
+
+    private void textViewController() {
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Intent to NewRegisterActivity
+                Intent intent = new Intent(MainActivity.this, NewRegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
-}
+
+    private void initialView() {
+
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+
+    }
+
+}  // Main Class นี่คือคลาสหลัก
