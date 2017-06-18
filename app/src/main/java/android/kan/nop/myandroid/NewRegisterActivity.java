bcbcbe3953203e1bf7cdd39d7ba7bcc235ee45ba
unnop <ukangkan@yahoo.com>
2017-06-18 14:09:24 +0700

    day 2 coding NewRegis Success

--- app/src/main/java/android/kan/nop/myandroid/NewRegisterActivity.java
+++ app/src/main/java/android/kan/nop/myandroid/NewRegisterActivity.java
@@ -2,12 +2,47 @@ package android.kan.nop.myandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class NewRegisterActivity extends AppCompatActivity {
+
+    //Explicit
+    private ImageView imageView;
+    private EditText nameEditText, userEditText, passwordEditText;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_register);
+
+        // Initial View
+        initialView();
+
+        // Back Controller
+        backController();
+
+    }   // Main Method
+
+    private void backController() {
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
-}
+
+    private void initialView() {
+        imageView = (ImageView) findViewById(R.id.btnBack);
+        nameEditText = (EditText) findViewById(R.id.edtName);
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        button = (Button) findViewById(R.id.btnNewRegister);
+    }
+
+}   // Main Class
